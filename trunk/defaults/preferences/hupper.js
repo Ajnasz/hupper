644716bbc2a7761c5288aa8021ae7718d9c5216d
ajnasz <devnull@localhost>
2007-09-02 16:24:58 +0200

    [svn] default trollcolor changed\ default huppercolor changed\ tags pref added

--- trunk/defaults/preferences/hupper.js
+++ trunk/defaults/preferences/hupper.js
@@ -1,13 +1,15 @@
 pref("extensions.hupper.filtertrolls", true);
 pref("extensions.hupper.trolls", '');
 pref("extensions.hupper.trollfiltermethod", 'hilight');
-pref("extensions.hupper.trollcolor", '#FF4300');
+pref("extensions.hupper.trollcolor", '#CFB2A7');
 
 pref("extensions.hupper.huppers", '');
 pref("extensions.hupper.filterhuppers", true);
-pref("extensions.hupper.huppercolor", '#00A21E');
+pref("extensions.hupper.huppercolor", '#B5D7BE');
 
 pref("extensions.hupper.replacenewcommenttext", true);
 pref("extensions.hupper.newcommenttext", '[new]');
 
 pref("extensions.hupper.prevnextlinks", true);
+
+pref("extensions.hupper.tags", "{}");
