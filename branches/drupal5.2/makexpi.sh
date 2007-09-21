706eed5000504fca22c42b9e14e8a34d360f8cd7
ajnasz <devnull@localhost>
2007-09-21 12:03:30 +0200

    [svn] branch letrehozasa drupal 5.2-hoz

---
+++ branches/drupal5.2/makexpi.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm chrome/hupper.jar hupper.xpi;
+
+cd chrome;
+zip -r hupper.jar content/* -x *.svn/*;
+zip -r hupper.jar locale/* -x *.svn/*;
+zip -r hupper.jar skin/* -x *.svn/*;
+cd ..;
+zip hupper.xpi chrome.manifest install.rdf chrome/hupper.jar defaults/preferences/hupper.js license.txt -x *.svn/*
