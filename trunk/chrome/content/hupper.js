86e78282829089b154c972bb5a3451e5b8583c05
ajnasz <devnull@localhost>
2007-09-24 16:10:15 +0200

    [svn] add option to enbale/disable permalink, many code cleaning and a bit faster

--- trunk/chrome/content/hupper.js
+++ trunk/chrome/content/hupper.js
@@ -114,22 +114,36 @@ HupperPrefs = {
   hilightForumLinsOnHover: function()
   {
     return this.prefManager.getBoolPref('extensions.hupper.hilightforumlinesonhover');
+  },
+  /**
+   * @return {Boolean}
+   */
+  insertPermalink: function()
+  {
+    return this.prefManager.getBoolPref('extensions.hupper.insertpermalink');
   }
-
 };
+/**
+ * collects the elements, which are has the specified className (cn) and childNodes of the specified node (par)
+ * @author Koszti Lajos [Ajnasz] http://ajnasz.hu ajnasz@ajnasz.hu 
+ * @param {Object} par parent element node
+ * @param {String} cn className
+ * @param {String} el element type
+ * @return {Array}
+ */
 var getElementsByClassName = function(par, cn, el)
 {
     if(typeof el == 'undefined')
     {
         el = 'div';
     }
-    var ts = par.getElementsByTagName(el), out = Array();
-    for(var i = 0; i < ts.length; i++)
+    var ts = par.getElementsByTagName(el), out = Array(), i;
+    for(i = 0; i < ts.length; i++)
     {
-        if(hasClass(ts[i],cn))
-        {
+      if(hasClass(ts[i],cn))
+      {
         out.push(ts[i]);
-        }
+      }
     }
     return out;
 };
@@ -141,21 +155,14 @@ var getElementsByClassName = function(par, cn, el)
  */
 var getComments = function()
 {
-  if(w.document)
-  {
-    var comments = w.document.getElementById('comments');
-  }
-  else
-  {
-    var comments = w.getElementById('comments');
-  }
-  if(!comments)
+  var COMS = w.getElementById('comments');
+  if(!COMS)
   {
     return false;
   }
-  var ds = comments.getElementsByTagName('div');
-  var header, footer, el, comments = Array(), newComm, parentComment, indentComments = Array(), newComments = Array();
-  for(var i = 0; i < ds.length; i++)
+  var ds = COMS.getElementsByTagName('div');
+  var header, footer, el, comments = Array(), newComm, parentComment, indentComments = Array(), newComments = Array(), dsl = ds.length, i;
+  for(i = 0; i < dsl; i++)
   {
     if(hasClass(ds[i], 'comment'))
     {
@@ -166,6 +173,7 @@ var getComments = function()
         comment: ds[i],
         header: header,
         footer: footer,
+        footerLinks: footer.getElementsByTagName('ul')[0],
         id: ds[i].previousSibling.previousSibling.id,
         indent: getIndent(ds[i]),
         user: (typeof header.childNodes[1] != 'undefined') ?  header.childNodes[1].innerHTML : header.innerHTML.replace(/[^\(]+\( ([^ ]+).*/, '$1'),
@@ -186,98 +194,6 @@ var getComments = function()
     }
   }
   return Array(comments, newComments, indentComments);
-};
-/**
- * hilights trolls comments
- * @param {Array} trolls array with trolls names
- */
-trollFilter = function(trolls)
-{
-  var cl = comments.length;
-  for(var i = 0; i < cl; i++)
-  {
-    if(trolls.inArray(comments[i].user))
-    {
-      addClass(comments[i].comment, ' trollComment');
-      addClass(comments[i].header, ' trollCommentHeader');
-    }
-  }
-};
-/**
- * hilights huppers comments
- * @param {Array} huppers array with huppers names
- */
-hupperFilter = function(huppers)
-{
-  var cl = comments.length;
-  for(var i = 0; i < cl; i++)
-  {
-    if(huppers.inArray(comments[i].user))
-    {
-      addClass(comments[i].comment, ' hupperComment');
-      addClass(comments[i].header, ' hupperCommentHeader');
-    }
-  }
-};
-/**
- * replaces the 'új' text in the new comments header
- */
-newCommentTextReplacer = function()
-{
-
-  var newComment = null;
-  var newCommentText = HupperPrefs.newcommenttext();
-  var cl = comments.length;
-  for(var i = 0; i < cl; i++)
-  {
-    if(comments[i].newComm)
-    {
-      comments[i].newComm.innerHTML = newCommentText;
-    }
-  }
-};
-/**
- * realises the tagger functionality for the hup.hu site
- * @note this Objcect isn't used
- */
-var hupperTagger = function()
-{
-  var tagsTXT = HupperPrefs.tags();
-  this.tags = eval('('+tagsTXT+')');
-  this.showTags();
-};
-
-hupperTagger.prototype =
-{
-  tags: {},
-  /**
-   * shows the tags in the users box
-   */
-  showTags: function()
-  {
-    var out = '<div id="tags" class="content"><h2 class="title">Tags</h2>';
-    var tagLinks, tl;
-    for (tag in this.tags)
-    {
-      tagLinks = this.tags[tag];
-      out += '<h4>'+tag+'</h4>';
-      out += '<ul>';
-      tl = tagLinks.length;
-      for(var i = 0; i < tl; i++)
-      {
-        out += '<li><a href="http://hup.hu'+tagLinks[i].url+'">'+tagLinks[i].name+'</a></li>';
-      }
-    }
-    out += '</div>';
-    if(w.document)
-    {
-      w.document.getElementById('block-user-1').innerHTML += out;
-    }
-    else
-    {
-      w.getElementById('block-user-1').innerHTML += out;
-    }
-  }
 };
 /**
  * @param {String,Number,Array,Object} value
@@ -301,7 +217,7 @@ Array.prototype.inArray = function(value)
 Array.prototype.unique = function(b)
 {
   var a = Array(), i, l = this.length;
-  for(var i = 0; i < l; i++)
+  for(i = 0; i < l; i++)
   {
     if(a.indexOf(this[i], 0, b) < 0)
     {
@@ -314,7 +230,7 @@ Array.prototype.unique = function(b)
  * parses all comment on the page and add class names, replaces the 'új' text, etc.
  * @author Koszti Lajos [Ajnasz] http://ajnasz.hu ajnasz@ajnasz.hu
  */
-parseComments = function()
+parseComments = function(comments, newComments, indentComments)
 {
   var newCommentText = HupperPrefs.newcommenttext();
   var replacenewcommenttext = HupperPrefs.replacenewcommenttext();
@@ -323,12 +239,11 @@ parseComments = function()
 
   var trolls = HupperPrefs.trolls();
   var filtertrolls = HupperPrefs.filtertrolls();
-  var filteredtrolls = Array();
 
   var huppers = HupperPrefs.huppers();
   var filterhuppers = HupperPrefs.filterhuppers();
   var extraCommentLinks = HupperPrefs.extraCommentLinks();
-  var filteredhuppers = Array();
+  var insertPermalink = HupperPrefs.insertPermalink();
 
   var firstLinkText = hupperBundles.getString('FirstLinkText');
   var lastLinkText = hupperBundles.getString('LastLinkText');
@@ -343,42 +258,45 @@ parseComments = function()
   var prevLink, nextLink;
   var newCT;
   
-  // filter trolls and huppers
-  var cl = comments.length;
-  for(var i = 0; i < cl; i++)
-  {
-    if(filtertrolls)
+  comments.map(
+    function(C)
     {
-      if(trolls.inArray(comments[i].user))
+      if(filtertrolls)
       {
-        addClass(comments[i].comment, ' trollComment');
-        addClass(comments[i].header, ' trollCommentHeader');
-        filteredtrolls.push(comments[i].user);
+        if(trolls.inArray(C.user))
+        {
+          addClass(C.comment, ' trollComment');
+          addClass(C.header, ' trollCommentHeader');
+        }
       }
-    }
-    if(filterhuppers)
-    {
-      if(huppers.inArray(comments[i].user))
+      if(filterhuppers)
       {
-        addClass(comments[i].comment, 'hupperComment');
-        addClass(comments[i].header, 'hupperCommentHeader');
-        filteredhuppers.push(comments[i].user);
+        if(huppers.inArray(C.user))
+        {
+          addClass(C.comment, 'hupperComment');
+          addClass(C.header, 'hupperCommentHeader');
+        }
       }
+      var newLinks = '';
+      if(extraCommentLinks)
+      {
+        newLinks += '<li><a href="#top">' + topLinkText + '</a></li><li><a href="javascript:history.back();">' + backLinkText + '</a></li>';
+      }
+      if(C.parent != -1)
+      {
+        newLinks += '<li><a href="#'+ C.parent +'">' + parentLinkText + '</a></li>';
+      }
+      if(insertPermalink)
+      {
+        newLinks += '<li><a href="#' + C.id + '">permalink</a></li>';
+      }
+      C.footerLinks.innerHTML += newLinks;
     }
-    if(extraCommentLinks)
-    {
-      comments[i].footer.getElementsByTagName('ul')[0].innerHTML += '<li><a href="#top">' + topLinkText + '</a></li><li><a href="javascript:history.back();">' + backLinkText + '</a></li>';
-    }
-    if(comments[i].parent != -1)
-    {
-      comments[i].footer.getElementsByTagName('ul')[0].innerHTML += '<li><a href="#'+ comments[i].parent +'">' + parentLinkText + '</a></li>';
-    }
-    comments[i].footer.getElementsByTagName('ul')[0].innerHTML += '<li><a href="#'+comments[i].id+'">permalink</a>';
-  }
+  );
   if(replacenewcommenttext || prevnextlinks)
   {
-    var ncl = newComments.length;
-    for(var i = 0; i < ncl; i++)
+    var ncl = newComments.length, i;
+    for(i = 0; i < ncl; i++)
     {
       newCT = '<span class="hnav">';
       if(replacenewcommenttext)
@@ -398,99 +316,7 @@ parseComments = function()
       }
       newComments[i].comment.removeChild(newComments[i].newComm);
       newComments[i].comment.innerHTML = newCT + '</span>' + newComments[i].comment.innerHTML;
-
     }
-  }
-  /*
-  if(filteredtrolls.length > 0)
-  {
-    filteredtrolls = filteredtrolls.unique();
-    var ftdiv = document.createElement('div');
-    ftdiv.id = 'filteredtrolls';
-    ftdiv.innerHTML = filteredtrolls.toString();
-    comments[0].table.parentNode.insertBefore(ftdiv, comments[0].table);
-    // document.getElementById('filteredtrolls').innerHTML = filteredtrolls.toString();
-    // comment[0].parentNode.insertBefore(ftdiv, comment[0]);
-  }
-  if(filteredhuppers.length > 0)
-  {
-    filteredhuppers = filteredhuppers.unique();
-    var fhdiv = document.createElement('div');
-    fhdiv.id = 'filteredhuppers';
-    fhdiv.innerHTML = filteredhuppers.toString();
-    comments[0].table.parentNode.insertBefore(fhdiv, comments[0].table);
-    // document.getElementById('filteredhuppers').innerHTML = filteredhuppers.toString();
-    // cc.parentNode.insertBefore(ftdiv, cc.nextSibling);
-  }
-  */
-};
-/**
- * add an extra #new str to the forums links
- * @author Koszti Lajos [Ajnasz] http://ajnasz.hu ajnasz@ajnasz.hu
- * @return {Boolean}
- */
-var forumNewMessageLink = function()
-{
-  this.getForumTable();
-  if(this.forumTable)
-  {
-    this.forumLinks = this.forumTable.getElementsByTagName('a');
-    this.forumLines = this.forumTable.getElementsByTagName('tr');
-    // this.modifyLinks();
-    if(HupperPrefs.hilightForumLinsOnHover())
-    {
-      this.addHoverToLines();
-    }
-    this.getForumTable();
-    return true;
-  }
-  return false;
-};
-forumNewMessageLink.prototype =
-{
-  forumTable: null,
-  forumLinks: null,
-  /**
-   * adds the #new string to the link
-   */
-  modifyLinks: function()
-  {
-    var fll = this.forumLinks.length;
-    for(var i = 0; i < fll; i+=3)
-    {
-      this.forumLinks[i].href += '#new';
-    }
-  },
-  /**
-   * adds hilight effect to the lines of the table
-   */
-  addHoverToLines: function()
-  {
-    var fll = this.forumLines.length;
-    for(var i = 0; i < fll; i++)
-    {
-      this.forumLines[i].addEventListener('mouseover', function()
-          {
-            addClass(this, 'forumHover');
-          }, false);
-      this.forumLines[i].addEventListener('mouseout', function()
-          {
-            removeClass(this, 'forumHover');
-          }, false);
-    }
-  },
-  /**
-   * @return {Boolean}
-   */
-  getForumTable: function()
-  {
-    var t = w.getElementsByTagName('table')[5];
-    if(th = t.getElementsByTagName('th')[0])
-    {
-      this.forumTable = t;
-      return true;
-    }
-    return false;
   }
 };
 /**
@@ -522,14 +348,7 @@ var removeClass = function(el, c)
  */
 var hasClass = function(el, c)
 {
-  /*
-  HLog.log(el + ' - ' + c);
-  if(typeof el == 'undefined')
-  {
-    HLog.log('ERROR: '+ c);
-  }
-  */
-  cl = new RegExp(c);
+ cl = new RegExp(c);
   return cl.test(el.className);
 };
 /**
@@ -551,27 +370,28 @@ var getParentComment = function(indentedComments, comment)
 };
 /**
  * @author Koszti Lajos [Ajnasz] http://ajnasz.hu ajnasz@ajnasz.hu
- * @return how indented the comment
  * @param {Object} el
+ * @return how indented the comment
  */
 var getIndent = function(el)
 {
-    var indent = 0;
-    while(hasClass(el.parentNode, 'indent'))
-    {
-        indent++;
-        el = el.parentNode;
-    }
-    return indent;
+  var indent = 0;
+  while(hasClass(el.parentNode, 'indent'))
+  {
+    indent++;
+    el = el.parentNode;
+  }
+  return indent;
 };
 
 /**
  * adds my own styles to the hup.hu header
- * @author Koszti Lajos [Ajnasz] http://ajnasz.hu ajnasz@ajnasz.hu * @param {Object} o event object
+ * @author Koszti Lajos [Ajnasz] http://ajnasz.hu ajnasz@ajnasz.hu *
+ * @param {Object} o event object
  */
 addHupStyles = function(o)
 {
-  var styles = '<style type="text/css">';
+  var styles = '<style type="text/css" id="hupperStyles">';
   // hupper styles
   switch(HupperPrefs.trollfiltermethod())
   {
@@ -598,7 +418,6 @@ addHupStyles = function(o)
   styles += '.hnew { color: red; font-weight: bold; }';
   styles += '.hnav { float: right; padding-right: 5px; padding-top: 2px; }';
   styles += '.submitted { padding: 2px !important;';
-  // hupper styles end
   styles += '</style>';
   w.getElementsByTagName('head')[0].innerHTML += styles;
 };
@@ -612,24 +431,8 @@ HUPPER = function(e)
   w = e.originalTarget;
   if(w.location.href.match(/^https?:\/\/(?:www\.)?hup\.hu/))
   {
-    if(w.document)
-    {
-      var body = w.document.getElementsByTagName('body')[0];
-      var p = w.document.getElementById('primary');
-      // var all = w.document.getElementById('all');
-      // var topNav = w.document.getElementById('top-nav');
-    }
-    else
-    {
-      var body = w.getElementsByTagName('body')[0];
-      var p = w.getElementById('primary');
-      // var all = w.getElementById('all')
-      // var topNav = w.getElementById('top-nav');
-    }
-//    body.innerHTML = '<a name="top"></a>' + body.innerHTML;
-    // var a = document.createElement('a');
-    // a.setAttribute('name', 'top');
-    // topNav.insertBefore(a, all);
+    var body = w.getElementsByTagName('body')[0];
+    var p = w.getElementById('primary');
     p.getElementsByTagName('a')[0].name = 'top';
     hupperBundles = document.getElementById('hupper-bundles');
     addHupStyles();
@@ -639,10 +442,8 @@ HUPPER = function(e)
       comments = c[0];
       newComments = c[1];
       indentComments = c[2];
-      parseComments();
+      parseComments(comments, newComments, indentComments);
     }
-    // new hupperTagger();
-    // new forumNewMessageLink();
     HLog.log('initialized');
   }
 };
