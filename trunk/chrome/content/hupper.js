0eb7958b08615f28bd58bae9d28c7324a3a947d3
ajnasz <devnull@localhost>
2007-10-02 00:16:45 +0200

    [svn] getElementsByClassName: if parent element undefined, set the body as parent getNodes: push the new nodes into a dedicated array parseNodes: use only DOM function, like setAttribute etc parseComments: replace the innerHTML functions to DOM functions addClass, removeClass, hasClass, addHupStyles: DOM functions

--- trunk/chrome/content/hupper.js
+++ trunk/chrome/content/hupper.js
@@ -111,7 +111,7 @@ HupperPrefs = {
   /**
    * @return {Boolean}
    */
-  hilightForumLinsOnHover: function()
+  hilightForumLinesOnHover: function()
   {
     return this.prefManager.getBoolPref('extensions.hupper.hilightforumlinesonhover');
   },
@@ -132,7 +132,7 @@ HupperPrefs = {
 };
 /**
  * collects the elements, which are has the specified className (cn) and childNodes of the specified node (par)
- * @author Koszti Lajos [Ajnasz] http://ajnasz.hu ajnasz@ajnasz.hu 
+ * @author Koszti Lajos [Ajnasz] http://ajnasz.hu ajnasz@ajnasz.hu
  * @param {Object} par parent element node
  * @param {String} cn className
  * @param {String} el element type
@@ -143,6 +143,10 @@ var getElementsByClassName = function(par, cn, el)
     if(typeof el == 'undefined')
     {
         el = 'div';
+    }
+    if(typeof par == 'undefined')
+    {
+      par = w.getElementsByTagName('body')[0];
     }
     var ts = par.getElementsByTagName(el), out = Array(), i;
     for(i = 0; i < ts.length; i++)
@@ -202,11 +206,15 @@ var getComments = function()
   }
   return Array(comments, newComments, indentComments);
 };
+/**
+ * @author Koszti Lajos [Ajnasz] http://ajnasz.hu ajnasz@ajnasz.hu
+ * @return {Array}
+ */
 var getNodes = function()
 {
   var c = w.getElementById('content-both');
   var ds = c.getElementsByTagName('div');
-  var nodes = Array(), node = {}, dsl = ds.length, i, title, submitData, content, footer;
+  var nodes = Array(), newnodes = Array(), node = {}, dsl = ds.length, i, title, submitData, content, footer;
   for(i = 0; i < dsl; i++)
   {
     if(hasClass(ds[i], 'node'))
@@ -220,93 +228,99 @@ var getNodes = function()
         submitData: submitData,
         content: content,
         footer: footer,
-        new: getElementsByClassName(footer,'comment_new_comments', 'li').length > 0 ? true : false
+        newc: getElementsByClassName(footer,'comment_new_comments', 'li').length > 0 ? true : false
       }
       nodes.push(node);
+      if(node.newc)
+      {
+        newnodes.push(node);
+      }
     }
   }
-  return nodes;
+  return Array(nodes, newnodes);
 };
+/**
+ * @author Koszti Lajos [Ajnasz] http://ajnasz.hu ajnasz@ajnasz.hu
+ * @param {Array} nodes
+ */
 var parseNodes = function(nodes)
 {
-  
+
   var newCommentText = HupperPrefs.newcommenttext();
-  var spa = document.createElement('span');
-  spa.setAttribute('style', 'color:red;float:right;font-size:11px;margin-top:-16px;');
-  /*
+  var spa = w.createElement('span');
+  spa.setAttribute('style', 'font-size:11px;');
+
   var firstLinkText = hupperBundles.getString('FirstLinkText');
   var lastLinkText = hupperBundles.getString('LastLinkText');
   var prevLinkText = hupperBundles.getString('PrevLinkText');
   var nextLinkText = hupperBundles.getString('NextLinkText');
 
-  var a = document.createElement('a');
-  span.setAttribute('style', 'float:right;font-size:10px;color:red;');
-  var fit = document.createTextNode(firstLinkText);
-  var lat = document.createTextNode(lastLinkText);
-  var prt = document.createTextNode(prevLinkText);
-  var net = document.createTextNode(nextLinkText);
-  */
-  var newCt = document.createTextNode(newCommentText);
+  var a = w.createElement('a');
+  var fit = w.createTextNode(firstLinkText);
+  var lat = w.createTextNode(lastLinkText);
+  var prt = w.createTextNode(prevLinkText);
+  var net = w.createTextNode(nextLinkText);
 
-  /*
+  var newCt = w.createTextNode(newCommentText);
+
+
   var lia = a.cloneNode(true);
   var nea = a.cloneNode(true);
-  nea.innerText = net;
-  // nea.appendChild(net);
+  nea.appendChild(net);
   var pra = a.cloneNode(true);
-  pra.innerText = prt;
-  */
-  // pra.appendChild(prt);
-  var sp, liaC, praC, neaC, fitC, latC;
+
+  pra.appendChild(prt);
+  var sp, liaC, praC, neaC, nsp;
   for(var i = 0; i < nodes.length; i++)
   {
-    if(nodes[i].new)
+    if(nodes[i].newc)
     {
       sp = spa.cloneNode(true);
-      /*
+      sp.setAttribute('class', 'nnew');
+
+      // append 'new' text
+      nsp = spa.cloneNode(true);
+      nsp.setAttribute('class', 'hnew');
+      nsp.appendChild(newCt.cloneNode(true));
+      sp.appendChild(nsp);
+
+      // create a name
       liaC = lia.cloneNode(true);
-      liaC.name = 'newhupnode'+i;
-      sp.appendChild(liaC);
+      liaC.setAttribute('name', 'newhupnode'+i);
+      nodes[i].title.parentNode.insertBefore(liaC, nodes[i].title);
 
       if(i > 0)
       {
         praC = pra.cloneNode(true);
-        praC.href = 'newhupnode' + (i-1);
+        praC.setAttribute('href', '#newhupnode' + (i-1));
         sp.appendChild(praC);
       }
       else
       {
-        fitC = lat.cloneNode(true);
-        sp.appendChild(fitC);
+        nsp = spa.cloneNode(true);
+        nsp.appendChild(fit);
+        sp.appendChild(nsp);
       }
       if(i < nodes.length-1)
       {
         neaC = nea.cloneNode(true);
-        neaC.href = 'newhupnode' + (i+1);
+        neaC.setAttribute('href', '#newhupnode' + (i+1));
         sp.appendChild(neaC);
       }
       else
       {
-        latC = lat.cloneNode(true);
-        sp.appendChild(latC);
+        nsp = spa.cloneNode(true);
+        nsp.appendChild(lat);
+        sp.appendChild(nsp);
       }
-      */
-      sp.appendChild(newCt.cloneNode(true));
-      nodes[i].title.appendChild(sp);
-
-      /*
-      t = '<a name="#newnode'+ i +'"></a><span style="float:right;font-size: 10px;color:red;">'; 
-      t += (i > 0) ? '<a href="#newnode'+(i-1) + '">' + prevLinkText + '</a>' : firstLinkText;
-      t += (i < nodes.length-1) ? '<a href="#newnode'+(i+1) + '">' + nextLinkText + '</a>' : nextLinkText;
-      t += newCommentText; 
-      t += '</span>';
-      nodes[i].title.innerHTML += t;
-      */
+      nodes[i].title.insertBefore(sp, nodes[i].title.firstChild);
     }
   }
 }
 /**
+ * @author Koszti Lajos [Ajnasz] http://ajnasz.hu ajnasz@ajnasz.hu
  * @param {String,Number,Array,Object} value
+ * @return {Boolean}
  */
 Array.prototype.inArray = function(value)
 {
@@ -321,7 +335,7 @@ Array.prototype.inArray = function(value)
   return false;
 };
 /**
- *
+ * @author Koszti Lajos [Ajnasz] http://ajnasz.hu ajnasz@ajnasz.hu
  * @return {Array}
  */
 Array.prototype.unique = function(b)
@@ -339,6 +353,9 @@ Array.prototype.unique = function(b)
 /**
  * parses all comment on the page and add class names, replaces the 'új' text, etc.
  * @author Koszti Lajos [Ajnasz] http://ajnasz.hu ajnasz@ajnasz.hu
+ * @param {Array} comments
+ * @param {Array} newComments
+ * @param {Array} indentComments
  */
 parseComments = function(comments, newComments, indentComments)
 {
@@ -363,11 +380,18 @@ parseComments = function(comments, newComments, indentComments)
   var backLinkText = hupperBundles.getString('BackLinkText');
   var parentLinkText = hupperBundles.getString('ParentLinkText');
 
-  var cc = document.getElementById('comment_controls');
-
   var prevLink, nextLink;
   var newCT;
-  
+
+
+  var listItem = w.createElement('li');
+  var linkItem = w.createElement('a');
+  var topTextItem = w.createTextNode(topLinkText);
+  var backTextItem = w.createTextNode(backLinkText);
+  var parentTextItem = w.createTextNode(parentLinkText);
+  var permaTextItem = w.createTextNode('permalink');
+
+  var tmpList, tmpLink, tmpTop, tmpBack, tmpParent, tmpPerma;
   comments.map(
     function(C)
     {
@@ -387,44 +411,111 @@ parseComments = function(comments, newComments, indentComments)
           addClass(C.header, 'hupperCommentHeader');
         }
       }
-      var newLinks = '';
       if(extraCommentLinks)
       {
-        newLinks += '<li><a href="#top">' + topLinkText + '</a></li><li><a href="javascript:history.back();">' + backLinkText + '</a></li>';
+        tmpList = listItem.cloneNode(true);
+        tmpLink = linkItem.cloneNode(true);
+        tmpLink.setAttribute('href', '#top');
+        tmpTop = topTextItem.cloneNode(true);
+
+        tmpLink.appendChild(tmpTop);
+        tmpList.appendChild(tmpLink);
+
+        C.footerLinks.appendChild(tmpList);
+
+        tmpList = listItem.cloneNode(true);
+        tmpLink = linkItem.cloneNode(true);
+        tmpLink.setAttribute('href', 'javascript:history.back();');
+        tmpBack = backTextItem.cloneNode(true);
+
+        tmpLink.appendChild(tmpBack);
+        tmpList.appendChild(tmpLink);
+
+        C.footerLinks.appendChild(tmpList);
       }
       if(C.parent != -1)
       {
-        newLinks += '<li><a href="#'+ C.parent +'">' + parentLinkText + '</a></li>';
+
+        tmpList = listItem.cloneNode(true);
+        tmpLink = linkItem.cloneNode(true);
+        tmpLink.setAttribute('href', '#' + C.parent);
+        tmpParent = parentTextItem.cloneNode(true);
+
+        tmpLink.appendChild(tmpParent);
+        tmpList.appendChild(tmpLink);
+
+        C.footerLinks.appendChild(tmpList);
       }
       if(insertPermalink)
       {
-        newLinks += '<li><a href="#' + C.id + '">permalink</a></li>';
+        tmpList = listItem.cloneNode(true);
+        tmpLink = linkItem.cloneNode(true);
+        tmpLink.setAttribute('href', '#' + C.id);
+        tmpPerma = permaTextItem.cloneNode(true);
+
+        tmpLink.appendChild(tmpPerma);
+        tmpList.appendChild(tmpLink);
+
+        C.footerLinks.appendChild(tmpList);
       }
-      C.footerLinks.innerHTML += newLinks;
     }
   );
   if(replacenewcommenttext || prevnextlinks)
   {
+    var spanNode = w.createElement('span');
+    var linkNode = w.createElement('a');
+    var newComTextNode = (replacenewcommenttext) ? w.createTextNode(newCommentText) : w.reateTextNode(newComments[0].newComm.innerHTML);
+    var prevLinkTextNode = w.createTextNode(prevLinkText);
+    var nextLinkTextNode = w.createTextNode(nextLinkText);
+
+    var tmpSpan1, tmpSpan2, tmpSpan3, tmpNewComText, tmpPrevLinkText, tmpNextLinkText, tmpLink;
     var ncl = newComments.length, i;
     for(i = 0; i < ncl; i++)
     {
-      newCT = '<span class="hnav">';
-      if(replacenewcommenttext)
-      {
-        newCT += '<span class="hnew">' + newCommentText + '</span>';
-      }
-      else
-      {
-        newCT += '<span class="hnew">' + newComments[i].newComm.innerHTML + '</span>';
-      }
+      tmpSpan1 = spanNode.cloneNode(true);
+      tmpSpan1.setAttribute('class', 'hnav');
+
+      tmpSpan2 = spanNode.cloneNode(true);
+      tmpSpan2.setAttribute('class', 'hnew');
+
+      tmpNewComText = newComTextNode.cloneNode(true);
+      tmpSpan2.appendChild(tmpNewComText);
+
       if(prevnextlinks)
       {
-        prevLink = (newComments[i-1]) ? ' <a href="#'+newComments[i-1].id+'">'+prevLinkText+'</a>' : ' ' + firstLinkText;
-        nextLink = (newComments[i+1]) ? ' <a href="#'+newComments[i+1].id+'">'+nextLinkText+'</a>' : ' ' + lastLinkText;
-        newCT += prevLink + nextLink;
+
+        if(newComments[i-1])
+        {
+          tmpLink = linkNode.cloneNode(true);
+          tmpLink.setAttribute('href', '#' + newComments[i-1].id);
+          tmpLink.appendChild(prevLinkTextNode.cloneNode(true));
+          tmpSpan1.appendChild(tmpLink);
+        }
+        else
+        {
+          tmpSpan3 = spanNode.cloneNode(true);
+          tmpSpan3.appendChild(w.createTextNode(firstLinkText));
+          tmpSpan1.appendChild(tmpSpan3);
+        }
+
+
+        if(newComments[i+1])
+        {
+          tmpLink = linkNode.cloneNode(true);
+          tmpLink.setAttribute('href', '#' + newComments[i+1].id);
+          tmpLink.appendChild(nextLinkTextNode.cloneNode(true));
+          tmpSpan1.appendChild(tmpLink);
+        }
+        else
+        {
+          tmpSpan3 = spanNode.cloneNode(true);
+          tmpSpan3.appendChild(w.createTextNode(lastLinkText));
+          tmpSpan1.appendChild(tmpSpan3);
+        }
       }
       newComments[i].comment.removeChild(newComments[i].newComm);
-      newComments[i].comment.innerHTML = newCT + '</span>' + newComments[i].comment.innerHTML;
+      tmpSpan1.appendChild(tmpSpan2);
+      newComments[i].header.insertBefore(tmpSpan1, newComments[i].header.firstChild);
     }
   }
 };
@@ -436,7 +527,15 @@ parseComments = function(comments, newComments, indentComments)
  */
 var addClass = function(el, c)
 {
-  el.className += ' ' + c;
+  var curClass = el.getAttribute('class');
+  if(curClass === null)
+  {
+    el.setAttribute('class', c);
+  }
+  else
+  {
+    el.setAttribute('class', curClass + ' ' + c);
+  }
 };
 /**
  * removes the specified class from the element
@@ -446,7 +545,7 @@ var addClass = function(el, c)
  */
 var removeClass = function(el, c)
 {
-  el.className = el.className.replace(c, '');
+  el.setAttribute('class', el.getAttribute('class').replace(c, ''));
 };
 /**
  * checks that the element has the specified class or not
@@ -457,8 +556,8 @@ var removeClass = function(el, c)
  */
 var hasClass = function(el, c)
 {
- cl = new RegExp(c);
-  return cl.test(el.className);
+  cl = new RegExp(c);
+  return cl.test(el.getAttribute('class'));
 };
 /**
  * @author Koszti Lajos [Ajnasz] http://ajnasz.hu ajnasz@ajnasz.hu
@@ -470,7 +569,7 @@ var getParentComment = function(indentedComments, comment)
 {
   if(comment.indent > 0)
   {
-    return indentedComments[comment.indent-1][indentedComments[comment.indent-1].length-1];
+    return indentedComments[(comment.indent-1)][(indentedComments[(comment.indent-1)].length-1)];
   }
   else
   {
@@ -500,42 +599,48 @@ var getIndent = function(el)
  */
 addHupStyles = function(o)
 {
-  var styles = '<style type="text/css">';
-  // hupper styles
+  var styles = '';
   switch(HupperPrefs.trollfiltermethod())
   {
     case 'hide':
       styles += '.trollComment {display:none !important;}';
     break;
     case 'hilight':
-      styles += '.trollCommentHeader {background-color:'+HupperPrefs.trollcolor()+' !important;}';
+      styles += '.trollCommentHeader {background-color:' + HupperPrefs.trollcolor() + ' !important;}';
     break;
     default:
-      styles += '.trollCommentHeader {background-color:'+HupperPrefs.trollcolor()+' !important;}';
+      styles += '.trollCommentHeader {background-color:'+HupperPrefs.trollcolor() + ' !important;}';
   }
-  styles += '.hupperCommentHeader {background-color: '+HupperPrefs.huppercolor()+' !important;}';
+  styles += '.hupperCommentHeader {background-color: '+HupperPrefs.huppercolor() + ' !important;}';
   styles += '#filteredhuppers, #filteredtrolls {display:block; !important;}';
   styles += '#tags {background-color:#F6F6EB; }';
   styles += '#tags h4 {margin: 0;padding:0; }';
   styles += '#tags ul {list-style:none;padding:0;margin:0;}#tags li {padding-left:5px;margin:0;}';
-  if(HupperPrefs.hilightForumLinsOnHover())
+  if(HupperPrefs.hilightForumLinesOnHover())
   {
     styles += 'tr.odd:hover td, tr.even:hover {background-color: #D8D8C4;}';
   }
   styles += 'input, textarea { border: 1px solid #999 !important; padding: 2px !important; margin-bottom: 5px !important; }';
   styles += 'input:focus, textarea:focus { border-color: #000 !important; }';
   styles += '.hnew { color: red; font-weight: bold; }';
-  styles += '.hnav { float: right; padding-right: 5px; padding-top: 2px; }';
-  styles += '.submitted { padding: 2px !important;';
-  styles += '</style>';
-  w.getElementsByTagName('head')[0].innerHTML += styles;
+  styles += '.nnew { float:right; }';
+  styles += '.nnew * { margin-left: 2px; margin-right: 2px; }';
+  styles += '.hnav { float: right; padding-right: 5px; }';
+  styles += '.hnav * { margin-left: 2px; margin-right: 2px; }';
+  styles += '.submitted { padding: 2px !important; }';
+
+  var st = w.createElement('style');
+  var sti = w.createTextNode(styles);
+  st.setAttribute('type', 'text/css');
+  st.appendChild(sti);
+  w.getElementsByTagName('head')[0].appendChild(st);
 };
 /**
  * initialization function, runs when the page is loaded
  * @author Koszti Lajos [Ajnasz] http://ajnasz.hu ajnasz@ajnasz.hu
  * @param {Object} e window load event object
  */
-HUPPER = function(e)
+var HUPPER = function(e)
 {
   w = e.originalTarget;
   if(w.location.href.match(/^https?:\/\/(?:www\.)?hup\.hu/))
@@ -545,7 +650,7 @@ HUPPER = function(e)
     var p = w.getElementById('primary');
     p.getElementsByTagName('a')[0].name = 'top';
     hupperBundles = document.getElementById('hupper-bundles');
-    if(document.getElementById('comments'))
+    if(w.getElementById('comments'))
     {
       var c = getComments();
       comments = c[0];
@@ -555,7 +660,7 @@ HUPPER = function(e)
     }
     else if(HupperPrefs.insertnewtexttonode())
     {
-      parseNodes(getNodes());
+      parseNodes(getNodes()[1]);
     }
 
     HLog.log('initialized');
