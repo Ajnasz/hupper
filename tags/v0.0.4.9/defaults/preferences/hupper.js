a44bbb962bc4d10f162fb40a0928952756b07741
ajnasz <devnull@localhost>
2007-11-11 20:38:59 +0100

    [svn] tagging 0.0.4.9

---
+++ tags/v0.0.4.9/defaults/preferences/hupper.js
@@ -0,0 +1,24 @@
+pref("extensions.hupper.filtertrolls", true);
+pref("extensions.hupper.trolls", '');
+pref("extensions.hupper.trollfiltermethod", 'hilight');
+pref("extensions.hupper.trollcolor", '#CFB2A7');
+
+pref("extensions.hupper.huppers", '');
+pref("extensions.hupper.filterhuppers", true);
+pref("extensions.hupper.huppercolor", '#B5D7BE');
+
+pref("extensions.hupper.replacenewcommenttext", true);
+pref("extensions.hupper.newcommenttext", '[new]');
+
+pref("extensions.hupper.prevnextlinks", true);
+
+pref("extensions.hupper.tags", "{}");
+pref("extensions.hupper.extracommentlinks", true);
+pref("extensions.hupper.hilightforumlinesonhover", true);
+
+pref("extensions.hupper.insertpermalink", true);
+
+pref("extensions.hupper.insertnewtexttonode", true);
+pref("extensions.hupper.fadeparentcomment", true);
+
+pref("extensions.hupper.showqnavbox", true);
