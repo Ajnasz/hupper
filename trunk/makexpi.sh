ffa40643c9e8fe47bee618ca12a4e86eb5d9c7a1
ajnasz <devnull@localhost>
2008-06-08 15:22:20 +0200

    [svn]  - remova install.js from the file list

--- trunk/makexpi.sh
+++ trunk/makexpi.sh
@@ -7,4 +7,4 @@ zip -r hupper.jar content/* -x \*.svn/\*;
 zip -r hupper.jar locale/* -x \*.svn/*;
 zip -r hupper.jar skin/* -x \*.svn/\*;
 cd ..;
-zip hupper.xpi chrome.manifest install.rdf install.js chrome/hupper.jar defaults/preferences/hupper.js license.txt -x \*.svn/\*
+zip hupper.xpi chrome.manifest install.rdf chrome/hupper.jar defaults/preferences/hupper.js license.txt -x \*.svn/\*
