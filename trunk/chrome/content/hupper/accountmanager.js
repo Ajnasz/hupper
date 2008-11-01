e037f3d08db9dc9354926a367e46f432f9937f87
ajnasz <devnull@localhost>
2008-11-01 14:26:42 +0100

    [svn]  - add separate preference getter/setter functions: add hp.js  - add accountmanager.js  - add passmanager.js

---
+++ trunk/chrome/content/hupper/accountmanager.js
@@ -0,0 +1,80 @@
+/**
+ * accountmanager.js
+ *
+ * @author Koszti Lajos [Ajnasz] http://ajnasz.hu ajnasz@ajnasz.hu
+ * @license GPL v2
+ * for more details see the license.txt file
+ */
+
+/**
+ * Google account manager namespace,
+ * check that the user is logged in,
+ * logging in the user
+ * @requires _passwordManager to get the users password
+ * @requires #getFeedList function, to gets the feeds
+ */
+var HupAccountManager = {
+  // mozilla nsi cookie manager component
+  CookieManager: Components.classes["@mozilla.org/cookiemanager;1"].getService(Components.interfaces.nsICookieManager),
+  /**
+   * Check, that the account is configured
+   * @type {Boolean}
+   */
+  passwordManager: function() {
+    return new _passwordManager();
+  },
+  hp: function() {
+    if(typeof hp == 'undefined') {
+      var hp = new HP();
+    }
+    return hp;
+  },
+  accountExists: function() {
+    if(this.hp().get.username() && this.passwordManager().getPassword()) {
+      return true;
+    }
+    return false;
+  },
+  /**
+   * do the login into the google service
+   * @param {Function} onLoad run after successful login
+   */
+  logIn: function(onLogin) {
+    if(this.accountExists()) {
+      var url = 'http://hup.hu/node?destination=node';
+      var param = 'name='+encodeURIComponent(this.hp().get.username())+'&pass='+encodeURIComponent(this.passwordManager().getPassword())+'&op=' + encodeURIComponent('Belépés') + '&form_id=user_login_block';
+      var _this = this;
+      new HupAjax({
+        url: url,
+        method: 'post',
+        successHandler: function(e) {
+          _this.ajaxSuccess(e);
+          if(typeof onLogin == 'function') {
+            onLogin();
+          }
+        },
+        pars: param
+      });
+    } else {
+      this.loginFailed();
+      return -1;
+    }
+    return true;
+  },
+  /**
+   * @param {Event} e event object
+   * @returns true if the login was succes and false if wasn't
+   * @type Boolean
+   */
+  ajaxSuccess: function(e) {
+    return true;
+  },
+  /**
+   * do things when the login failed
+   * @returns false
+   * @type Boolean
+   */
+  loginFailed: function() {
+    return false;
+  }
+};
---
+++ trunk/chrome/content/hupper/hp.js
@@ -0,0 +1,147 @@
+HP = function() {
+  this.M = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
+  this.get.M = this.set.M = this.M;
+};
+HP.prototype = {
+  M: null, // Manager
+  get: {
+    trolls: function() {
+      return this.M.getCharPref('extensions.hupper.trolls');
+    },
+    /**
+    * @return {Boolean}
+    */
+    filtertrolls: function() {
+      return this.M.getBoolPref('extensions.hupper.filtertrolls');
+    },
+    trollcolor: function() {
+      return this.M.getCharPref('extensions.hupper.trollcolor');
+    },
+    /**
+    * @return hide, hilight
+    */
+    trollfiltermethod: function() {
+      // hide, hilight
+      return this.M.getCharPref('extensions.hupper.trollfiltermethod');
+    },
+    /**
+    * @return {String}
+    */
+    huppers: function() {
+      return this.M.getCharPref('extensions.hupper.huppers');
+    },
+    filterhuppers: function() {
+      return this.M.getBoolPref('extensions.hupper.filterhuppers');
+    },
+    huppercolor: function() {
+      return this.M.getCharPref('extensions.hupper.huppercolor');
+    },
+    replacenewcommenttext: function() {
+      return this.M.getBoolPref('extensions.hupper.replacenewcommenttext');
+    },
+    newcommenttext: function() {
+      return this.M.getCharPref('extensions.hupper.newcommenttext');
+    },
+    extracommentlinks: function() {
+      return this.M.getBoolPref('extensions.hupper.extracommentlinks');
+    },
+    hilightforumlinesonhover: function() {
+      return this.M.getBoolPref('extensions.hupper.hilightforumlinesonhover');
+    },
+    insertpermalink: function() {
+      return this.M.getBoolPref('extensions.hupper.insertpermalink');
+    },
+    insertnewtexttonode: function() {
+      return this.M.getBoolPref('extensions.hupper.insertnewtexttonode');
+    },
+    fadeparentcomment: function() {
+      return this.M.getBoolPref('extensions.hupper.fadeparentcomment');
+    },
+    showqnavbox: function() {
+      return this.M.getBoolPref('extensions.hupper.showqnavbox');
+    },
+    hidetrollanswers: function() {
+      return this.M.getBoolPref('extensions.hupper.hidetrollanswers');
+    },
+    hideads: function() {
+      return this.M.getBoolPref('extensions.hupper.hideads');
+    },
+    highlightusers: function() {
+      return this.M.getCharPref('extensions.hupper.highlightusers');
+    },
+    username: function() {
+      return this.M.getCharPref('extensions.hupper.username');
+    }
+  },
+  set: {
+    M: this.M,
+    trolls: function(value) {
+      this.M.setCharPref('extensions.hupper.trolls', value);
+    },
+    /**
+    * @return {Boolean}
+    */
+    filtertrolls: function(value) {
+      this.M.setBoolPref('extensions.hupper.filtertrolls', value);
+    },
+    /**
+    * @return hide, hilight
+    */
+    trollfiltermethod: function(value) {
+      // hide, hilight
+      this.M.setCharPref('extensions.hupper.trollfiltermethod', value);
+    },
+    trollcolor: function(value) {
+      this.M.setCharPref('extensions.hupper.trollcolor', value);
+    },
+    /**
+    * @return {String}
+    */
+    huppers: function(value) {
+      this.M.setCharPref('extensions.hupper.huppers', value);
+    },
+    filterhuppers: function(value) {
+      this.M.setBoolPref('extensions.hupper.filterhuppers', value);
+    },
+    huppercolor: function(value) {
+      this.M.setCharPref('extensions.hupper.huppercolor', value);
+    },
+    replacenewcommenttext: function(value) {
+      this.M.setBoolPref('extensions.hupper.replacenewcommenttext', value);
+    },
+    newcommenttext: function(value) {
+      this.M.setCharPref('extensions.hupper.newcommenttext', value);
+    },
+    extracommentlinks: function(value) {
+      this.M.setBoolPref('extensions.hupper.extracommentlinks', value);
+    },
+    hilightforumlinesonhover: function(value) {
+      this.M.setBoolPref('extensions.hupper.hilightforumlinesonhover', value);
+    },
+    insertpermalink: function(value) {
+      this.M.setBoolPref('extensions.hupper.insertpermalink', value);
+    },
+    insertnewtexttonode: function(value) {
+      this.M.setBoolPref('extensions.hupper.insertnewtexttonode', value);
+    },
+    fadeparentcomment: function(value) {
+      this.M.setBoolPref('extensions.hupper.fadeparentcomment', value);
+    },
+    showqnavbox: function(value) {
+      this.M.setBoolPref('extensions.hupper.showqnavbox', value);
+    },
+    hidetrollanswers: function(value) {
+      this.M.setBoolPref('extensions.hupper.hidetrollanswers', value);
+    },
+    hideads: function(value) {
+      this.M.setBoolPref('extensions.hupper.hideads', value);
+    },
+    highlightusers: function(value) {
+      this.M.setCharPref('extensions.hupper.highlightusers', value);
+    },
+    username: function(value) {
+      return this.M.setCharPref('extensions.hupper.username', value);
+    }
+  }
+};
+
