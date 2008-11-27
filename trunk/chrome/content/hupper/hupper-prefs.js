6361647a11c22a4393f54e34b89a2d24e3257b26
Lajos Koszti <ajnasz@ajnasz.hu>
2008-11-27 23:56:09 +0100

    - make preference field id's more unique

--- trunk/chrome/content/hupper/hupper-prefs.js
+++ trunk/chrome/content/hupper/hupper-prefs.js
@@ -3,65 +3,67 @@
  * @licence General Public Licence v2
  */
 setPrefWinVals = function() {
-  document.getElementById('enable-trollfilter').checked = hp.get.filtertrolls();
-  document.getElementById('trolls').value = hp.get.trolls();
-  document.getElementById('troll-color').value = hp.get.trollcolor();
-  document.getElementById('trollfilter-method').value = hp.get.trollfiltermethod();
-  document.getElementById('enable-hupperfilter').checked = hp.get.filterhuppers();
-  document.getElementById('huppers').value = hp.get.huppers();
-  document.getElementById('hupper-color').value = hp.get.huppercolor();
-  document.getElementById('enable-new-comment-changer').checked = hp.get.replacenewcommenttext();
-  document.getElementById('new-comment-text').value = hp.get.newcommenttext();
-  document.getElementById('enable-extra-comment-links').checked = hp.get.extracommentlinks();
-  document.getElementById('hilight-forum-lines-onhover').checked = hp.get.hilightforumlinesonhover();
-  document.getElementById('insert-permalink').checked = hp.get.insertpermalink();
-  document.getElementById('insert-new-text-to-node').checked = hp.get.insertnewtexttonode();
-  document.getElementById('fade-parent-comment').checked = hp.get.fadeparentcomment();
-  document.getElementById('show-quick-nav-box').checked = hp.get.showqnavbox();
-  document.getElementById('hide-troll-answers').checked = hp.get.hidetrollanswers();
-  document.getElementById('hupper-highlightusers').value = hp.get.highlightusers();
-  document.getElementById('hide-taxonomy').value = hp.get.hidetaxonomy();
-  //document.getElementById('hupper-password').value = new _HUPPasswordManager().getPassword();
-  //document.getElementById('hupper-username').value = hp.get.username();
+  document.getElementById('HUP-enable-trollfilter').checked = hp.get.filtertrolls();
+  document.getElementById('HUP-trolls').value = hp.get.trolls();
+  document.getElementById('HUP-troll-color').value = hp.get.trollcolor();
+  document.getElementById('HUP-trollfilter-method').value = hp.get.trollfiltermethod();
+  document.getElementById('HUP-enable-hupperfilter').checked = hp.get.filterhuppers();
+  document.getElementById('HUP-huppers').value = hp.get.huppers();
+  document.getElementById('HUP-hupper-color').value = hp.get.huppercolor();
+  document.getElementById('HUP-enable-new-comment-changer').checked = hp.get.replacenewcommenttext();
+  document.getElementById('HUP-new-comment-text').value = hp.get.newcommenttext();
+  document.getElementById('HUP-enable-extra-comment-links').checked = hp.get.extracommentlinks();
+  document.getElementById('HUP-hilight-forum-lines-onhover').checked = hp.get.hilightforumlinesonhover();
+  document.getElementById('HUP-insert-permalink').checked = hp.get.insertpermalink();
+  document.getElementById('HUP-insert-new-text-to-node').checked = hp.get.insertnewtexttonode();
+  document.getElementById('HUP-fade-parent-comment').checked = hp.get.fadeparentcomment();
+  document.getElementById('HUP-show-quick-nav-box').checked = hp.get.showqnavbox();
+  document.getElementById('HUP-hide-troll-answers').checked = hp.get.hidetrollanswers();
+  document.getElementById('HUP-hupper-highlightusers').value = hp.get.highlightusers();
+  document.getElementById('HUP-hide-taxonomy').value = hp.get.hidetaxonomy();
+  document.getElementById('HUP-show-in-statusbar').checked = hp.get.showinstatusbar();
+  //document.getElementById('HUP-hupper-password').value = new _HUPPasswordManager().getPassword();
+  //document.getElementById('HUP-hupper-username').value = hp.get.username();
 };
 savePreferences = function() {
   if(!checkHLUsers()) {return false;}
-  hp.set.filtertrolls(document.getElementById('enable-trollfilter').checked);
-  hp.set.trolls(document.getElementById('trolls').value);
-  hp.set.trollcolor(document.getElementById('troll-color').value);
-  hp.set.trollfiltermethod(document.getElementById('trollfilter-method').value);
-  hp.set.filterhuppers(document.getElementById('enable-hupperfilter').checked);
-  hp.set.huppers(document.getElementById('huppers').value);
-  hp.set.huppercolor(document.getElementById('hupper-color').value);
-  hp.set.replacenewcommenttext(document.getElementById('enable-new-comment-changer').checked);
-  hp.set.newcommenttext(document.getElementById('new-comment-text').value);
-  hp.set.extracommentlinks(document.getElementById('enable-extra-comment-links').checked);
-  hp.set.hilightforumlinesonhover(document.getElementById('hilight-forum-lines-onhover').checked);
-  hp.set.insertpermalink(document.getElementById('insert-permalink').checked);
-  hp.set.insertnewtexttonode(document.getElementById('insert-new-text-to-node').checked);
-  hp.set.fadeparentcomment(document.getElementById('fade-parent-comment').checked);
-  hp.set.showqnavbox(document.getElementById('show-quick-nav-box').checked);
-  hp.set.hidetrollanswers(document.getElementById('hide-troll-answers').checked);
-  hp.set.highlightusers(document.getElementById('hupper-highlightusers').value);
-  hp.set.hidetaxonomy(document.getElementById('hide-taxonomy').value);
-  //hp.set.username(document.getElementById('hupper-username').value);
-  //new _HUPPasswordManager().addPassword(document.getElementById('hupper-password').value);
+  hp.set.filtertrolls(document.getElementById('HUP-enable-trollfilter').checked);
+  hp.set.trolls(document.getElementById('HUP-trolls').value);
+  hp.set.trollcolor(document.getElementById('HUP-troll-color').value);
+  hp.set.trollfiltermethod(document.getElementById('HUP-trollfilter-method').value);
+  hp.set.filterhuppers(document.getElementById('HUP-enable-hupperfilter').checked);
+  hp.set.huppers(document.getElementById('HUP-huppers').value);
+  hp.set.huppercolor(document.getElementById('HUP-hupper-color').value);
+  hp.set.replacenewcommenttext(document.getElementById('HUP-enable-new-comment-changer').checked);
+  hp.set.newcommenttext(document.getElementById('HUP-new-comment-text').value);
+  hp.set.extracommentlinks(document.getElementById('HUP-enable-extra-comment-links').checked);
+  hp.set.hilightforumlinesonhover(document.getElementById('HUP-hilight-forum-lines-onhover').checked);
+  hp.set.insertpermalink(document.getElementById('HUP-insert-permalink').checked);
+  hp.set.insertnewtexttonode(document.getElementById('HUP-insert-new-text-to-node').checked);
+  hp.set.fadeparentcomment(document.getElementById('HUP-fade-parent-comment').checked);
+  hp.set.showqnavbox(document.getElementById('HUP-show-quick-nav-box').checked);
+  hp.set.hidetrollanswers(document.getElementById('HUP-hide-troll-answers').checked);
+  hp.set.highlightusers(document.getElementById('HUP-hupper-highlightusers').value);
+  hp.set.hidetaxonomy(document.getElementById('HUP-hide-taxonomy').value);
+  hp.set.showinstatusbar(document.getElementById('HUP-show-in-statusbar').checked);
+  //hp.set.username(document.getElementById('HUP-hupper-username').value);
+  //new _HUPPasswordManager().addPassword(document.getElementById('HUP-hupper-password').value);
   return true;
 };
 var disableFields = function() {
-  document.getElementById('trollfilter-method').addEventListener('command', onChangeFilterMethod, false);
+  document.getElementById('HUP-trollfilter-method').addEventListener('command', onChangeFilterMethod, false);
 };
 var onChangeFilterMethod = function() {
-  if(document.getElementById('trollfilter-method').value == 'hide') {
-    document.getElementById('hide-troll-answers').disabled = false;
+  if(document.getElementById('HUP-trollfilter-method').value == 'hide') {
+    document.getElementById('HUP-hide-troll-answers').disabled = false;
   } else {
-    document.getElementById('hide-troll-answers').disabled = true;
+    document.getElementById('HUP-hide-troll-answers').disabled = true;
   }
 };
 var checkHLUsers = function() {
-  var hlUsers = document.getElementById('hupper-highlightusers').value.split(',');
-  var trolls = document.getElementById('troll-color').value.split(',');
-  var huppers = document.getElementById('huppers').value.split(',');
+  var hlUsers = document.getElementById('HUP-hupper-highlightusers').value.split(',');
+  var trolls = document.getElementById('HUP-troll-color').value.split(',');
+  var huppers = document.getElementById('HUP-huppers').value.split(',');
   var hlUsersObj = {};
   for(var i = 0, hl = hlUsers.length, hlUser; i < hl; i++) {
     hlUser = hlUsers[i].split(':');
