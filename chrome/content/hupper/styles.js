896276cfdae653c7b4c27dbf4769f84b688a8401
Lajos Koszti <ajnasz@ajnasz.hu>
2010-11-29 07:42:43 +0100

    load all styles with the Hupper.styles
    Using the styleLoader module

--- chrome/content/hupper/styles.js
+++ chrome/content/hupper/styles.js
@@ -1,45 +1,59 @@
 Hupper.styles = function() {
 
-  var indentStyle = 'chrome://hupper/skin/indentstyles.css',
-      accesibilityStyle = 'chrome://hupper/skin/accesibilitystyles.css';
-  var marker = '/* hupper width */';
-  var widthStyle = function(width) {
-    return '' +
-      marker + '@-moz-document url-prefix(http://hup.hu) {' +
-        '.sidebar {' +
-          'width:' + width + 'px !important;' +
-      '}' +
-    '}';
-  };
-
   HUP.L.log('init styles');
   if(!Hupper.styleLoader) {
     Components.utils.import('resource://huppermodules/styleLoader.jsm');
     var styleLoader = new StyleLoader();
     Hupper.styleLoader = styleLoader;
   }
-  // styleLoader.load('@-moz-document url-prefix(http://hup.hu) { #all {font-size:20px !important;} }');
-  if(HUP.hp.get.styleIndent()) {
-    Hupper.styleLoader.load(indentStyle);
-  } else {
-    Hupper.styleLoader.unLoad(indentStyle);
-  }
-  if(HUP.hp.get.styleAccessibility()) {
-    Hupper.styleLoader.load(accesibilityStyle);
-  } else {
-    Hupper.styleLoader.unLoad(accesibilityStyle);
-  }
-  var loadedStyles = Hupper.styleLoader.getLoadedStyles();
-  loadedStyles.forEach(function(style) {
-    HUP.L.log(style);
-    if(style.indexOf(marker) > -1) {
-      Hupper.styleLoader.unLoad(style);
-    }
-  });
-  var width = HUP.hp.get.styleWiderSidebar();
-  if(width > 0) {
-    Hupper.styleLoader.load(widthStyle(width));
-  }
+  var loadStyles = function(HUP, Hupper) {
+    return function() {
 
-  HUP.L.log('init styles finished');
+      var styles = '/* headers-answers-trolls */ @-moz-document url-prefix(http://hup.hu) {';
+      switch(HUP.hp.get.trollfiltermethod()) {
+        case 'hide':
+          styles += '.' + HUP.hp.get.trollCommentClass + ' {display:none !important;}';
+          if(HUP.hp.get.hidetrollanswers()) {
+            styles += '.' + HUP.hp.get.trollCommentAnswersClass + ' {display:none !important;}';
+          }
+          break;
+        case 'hilight':
+        default:
+          styles += '.' + HUP.hp.get.trollCommentHeaderClass + ' {background-color:' + HUP.hp.get.trollcolor() + ' !important;}';
+          break;
+      };
+      if(HUP.hp.get.hilightforumlinesonhover()) {
+        styles += 'tr.odd:hover td, tr.even:hover td {background-color: #D8D8C4;}';
+      }
+      styles += '}';
+      var indentStyle = 'chrome://hupper/skin/indentstyles.css',
+          accesibilityStyle = 'chrome://hupper/skin/accesibilitystyles.css';
+      var marker = '/* hupper width */';
+      var widthStyle = function(width) {
+        return '' +
+          marker + '@-moz-document url-prefix(http://hup.hu) {' +
+            '.sidebar {' +
+              'width:' + width + 'px !important;' +
+          '}' +
+        '}';
+      }
+
+
+      // styleLoader.load('@-moz-document url-prefix(http://hup.hu) { #all {font-size:20px !important;} }');
+      if(HUP.hp.get.styleIndent()) {
+        Hupper.styleLoader.load(indentStyle);
+      }
+      if(HUP.hp.get.styleAccessibility()) {
+        Hupper.styleLoader.load(accesibilityStyle);
+      }
+      var width = HUP.hp.get.styleWiderSidebar();
+      if(width > 0) {
+        Hupper.styleLoader.load(widthStyle(width));
+      }
+
+      HUP.L.log('init styles finished');
+      Hupper.styleLoader.load(styles);
+      Hupper.styleLoader.load('chrome://hupper/skin/hupper.css');
+  }}(HUP, Hupper);
+  Hupper.styleLoader.unloadAll(loadStyles);
 };
