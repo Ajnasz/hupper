b7dab977c705c17a39374a56beac5ef998531f6b
Lajos Koszti <ajnasz@ajnasz.hu>
2011-10-11 21:48:05 +0200

    Need a new head in hg

--- makexpi.sh
+++ makexpi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ########################## Configuration ################################
 if [ -z $1 ];then
-  VER='1.4a'`date '+%Y%m%d%H%M%S'`;
+  VER='1.4b'`date '+%Y%m%d%H%M%S'`;
 else
   VER=$1;
 fi
