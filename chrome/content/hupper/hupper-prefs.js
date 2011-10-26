90c62ad92ef1a3c97e8c4601c35335ab3615ba11
Lajos Koszti <ajnasz@ajnasz.hu>
2011-10-26 21:02:05 +0200

    Make possible to disable to show all links for a node

--- chrome/content/hupper/hupper-prefs.js
+++ chrome/content/hupper/hupper-prefs.js
@@ -1,39 +1,63 @@
+/*global Hupper: true, HUP: true*/
 /**
  * @author Koszti Lajos [Ajnasz] http://ajnasz.hu ajnasz@ajnasz.hu
  * @licence General Public Licence v2
  */
-Hupper.setPrefWinVals = function() {
-  document.getElementById('HUP-enable-trollfilter').checked = HUP.hp.get.filtertrolls();
-  document.getElementById('HUP-trolls').value = HUP.hp.get.trolls();
-  document.getElementById('HUP-troll-color').value = HUP.hp.get.trollcolor();
-  document.getElementById('HUP-trollfilter-method').value = HUP.hp.get.trollfiltermethod();
-  // document.getElementById('HUP-enable-hupperfilter').checked = HUP.hp.get.filterhuppers();
-  // document.getElementById('HUP-huppers').value = HUP.hp.get.huppers();
-  // document.getElementById('HUP-hupper-color').value = HUP.hp.get.huppercolor();
-  document.getElementById('HUP-enable-new-comment-changer').checked = HUP.hp.get.replacenewcommenttext();
-  document.getElementById('HUP-new-comment-text').value = HUP.hp.get.newcommenttext();
-  document.getElementById('HUP-enable-extra-comment-links').checked = HUP.hp.get.extracommentlinks();
-  document.getElementById('HUP-hilight-forum-lines-onhover').checked = HUP.hp.get.hilightforumlinesonhover();
-  document.getElementById('HUP-insert-permalink').checked = HUP.hp.get.insertpermalink();
-  document.getElementById('HUP-insert-new-text-to-node').checked = HUP.hp.get.insertnewtexttonode();
-  document.getElementById('HUP-fade-parent-comment').checked = HUP.hp.get.fadeparentcomment();
-  document.getElementById('HUP-show-quick-nav-box').checked = HUP.hp.get.showqnavbox();
-  document.getElementById('HUP-hide-troll-answers').checked = HUP.hp.get.hidetrollanswers();
-  document.getElementById('HUP-hupper-highlightusers').value = HUP.hp.get.highlightusers();
-  document.getElementById('HUP-hide-taxonomy').value = HUP.hp.get.hidetaxonomy();
-  document.getElementById('HUP-show-in-statusbar').checked = HUP.hp.get.showinstatusbar();
-  document.getElementById('HUP-parseblocks').checked = HUP.hp.get.parseblocks();
-
-  document.getElementById('HUP-style-indent').checked = HUP.hp.get.styleIndent();
-  document.getElementById('HUP-style-accessibility').checked = HUP.hp.get.styleAccessibility();
-  document.getElementById('HUP-style-sidebar-width').value = HUP.hp.get.styleWiderSidebar();
-  document.getElementById('HUP-style-min-fontsize').value = HUP.hp.get.styleMinFontsize();
-
-  //document.getElementById('HUP-hupper-password').value = new _HUPPasswordManager().getPassword();
-  //document.getElementById('HUP-hupper-username').value = HUP.hp.get.username();
+Hupper.prefItems = [
+    {id: 'HUP-enable-trollfilter', prefName: 'filtertrolls'},
+    {id: 'HUP-trolls', prefName: 'trolls'},
+    {id: 'HUP-troll-color', prefName: 'trollcolor'},
+    {id: 'HUP-trollfilter-method', prefName: 'trollfiltermethod'},
+    // {id: 'HUP-enable-hupperfilter', prefName: 'filterhuppers'},
+    // {id: 'HUP-huppers', prefName: 'huppers'},
+    // {id: 'HUP-hupper-color', prefName: 'huppercolor'},
+    {id: 'HUP-enable-new-comment-changer', prefName: 'replacenewcommenttext'},
+    {id: 'HUP-new-comment-text', prefName: 'newcommenttext'},
+    {id: 'HUP-enable-extra-comment-links', prefName: 'extracommentlinks'},
+    {id: 'HUP-hilight-forum-lines-onhover', prefName: 'hilightforumlinesonhover'},
+    {id: 'HUP-insert-permalink', prefName: 'insertpermalink'},
+    {id: 'HUP-insert-new-text-to-node', prefName: 'insertnewtexttonode'},
+    {id: 'HUP-fade-parent-comment', prefName: 'fadeparentcomment'},
+    {id: 'HUP-show-quick-nav-box', prefName: 'showqnavbox'},
+    {id: 'HUP-hide-troll-answers', prefName: 'hidetrollanswers'},
+    {id: 'HUP-hupper-highlightusers', prefName: 'highlightusers'},
+    {id: 'HUP-hide-taxonomy', prefName: 'hidetaxonomy'},
+    {id: 'HUP-show-in-statusbar', prefName: 'showinstatusbar'},
+    {id: 'HUP-parseblocks', prefName: 'parseblocks'},
+    {id: 'HUP-style-indent', prefName: 'styleIndent'},
+    {id: 'HUP-style-accessibility', prefName: 'styleAccessibility'},
+    {id: 'HUP-style-sidebar-width', prefName: 'styleWiderSidebar'},
+    {id: 'HUP-style-min-fontsize', prefName: 'styleMinFontsize'},
+    {id: 'HUP-set-unlimited-links', prefName: 'setunlimitedlinks'}
+    // {id: 'HUP-hupper-password', prefName: 'getPassword'},
+    // {id: 'HUP-hupper-username', prefName: 'username'}
+];
+Hupper.setPrefWinVals = function () {
+    Hupper.prefItems.forEach(function (item) {
+        var elem = document.getElementById(item.id),
+            value = HUP.hp.get[item.prefName]();
+        if (elem.nodeName === 'checkbox') {
+            elem.checked = value;
+        } else {
+            elem.value = value;
+        }
+    });
 };
-Hupper.savePreferences = function() {
-  if(!Hupper.checkHLUsers()) {return false;}
+Hupper.savePreferences = function () {
+    if (!Hupper.checkHLUsers()) {
+        return false;
+    }
+    Hupper.prefItems.forEach(function (item) {
+        var elem = document.getElementById(item.id),
+            value;
+        if (elem.nodeName === 'checkbox') {
+            value = elem.checked;
+        } else {
+            value = value;
+        }
+        value = HUP.hp.set[item.prefName](value);
+    });
+  /*
   HUP.hp.set.filtertrolls(document.getElementById('HUP-enable-trollfilter').checked);
   HUP.hp.set.trolls(document.getElementById('HUP-trolls').value);
   HUP.hp.set.trollcolor(document.getElementById('HUP-troll-color').value);
@@ -54,33 +78,32 @@ Hupper.savePreferences = function() {
   HUP.hp.set.hidetaxonomy(document.getElementById('HUP-hide-taxonomy').value);
   HUP.hp.set.showinstatusbar(document.getElementById('HUP-show-in-statusbar').checked);
   HUP.hp.set.parseblocks(document.getElementById('HUP-parseblocks').checked);
-
   HUP.hp.set.styleIndent(document.getElementById('HUP-style-indent').checked);
   HUP.hp.set.styleAccessibility(document.getElementById('HUP-style-accessibility').checked);
   HUP.hp.set.styleWiderSidebar(document.getElementById('HUP-style-sidebar-width').value);
   HUP.hp.set.styleMinFontsize(document.getElementById('HUP-style-min-fontsize').value);
-
-
   //HUP.hp.set.username(document.getElementById('HUP-hupper-username').value);
   //new _HUPPasswordManager().addPassword(document.getElementById('HUP-hupper-password').value);
-  var hideIcon = !HUP.hp.get.showinstatusbar();
-  Hupper.mapWindows(function(win) {
-    win.document.getElementById('HUP-statusbar').hidden = hideIcon;
-  })
-  Hupper.styles();
-  return true;
+  */
+    var hideIcon = !HUP.hp.get.showinstatusbar();
+    Hupper.mapWindows(function (win) {
+        win.document.getElementById('HUP-statusbar').hidden = hideIcon;
+    });
+    Hupper.styles();
+    return true;
 };
-Hupper.disableFields = function() {
-  document.getElementById('HUP-trollfilter-method').addEventListener('command', Hupper.onChangeFilterMethod, false);
+Hupper.disableFields = function () {
+    document.getElementById('HUP-trollfilter-method')
+      .addEventListener('command', Hupper.onChangeFilterMethod, false);
 };
-Hupper.onChangeFilterMethod = function() {
-  if(document.getElementById('HUP-trollfilter-method').value == 'hide') {
-    document.getElementById('HUP-hide-troll-answers').disabled = false;
-  } else {
-    document.getElementById('HUP-hide-troll-answers').disabled = true;
-  }
+Hupper.onChangeFilterMethod = function () {
+    if (document.getElementById('HUP-trollfilter-method').value === 'hide') {
+        document.getElementById('HUP-hide-troll-answers').disabled = false;
+    } else {
+        document.getElementById('HUP-hide-troll-answers').disabled = true;
+    }
 };
-Hupper.checkHLUsers = function() {
+Hupper.checkHLUsers = function () {
   var hlUsers = document.getElementById('HUP-hupper-highlightusers').value.split(',');
   var trolls = document.getElementById('HUP-trolls').value.split(',');
   // var huppers = document.getElementById('HUP-huppers').value.split(',');
