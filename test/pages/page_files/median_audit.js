138c41f29e91382b4b09c1814f7bdae844966a4e
Lajos Koszti <ajnasz@ajnasz.hu>
2015-02-22 19:43:34 +0100

    Remove js file contents

--- test/pages/page_files/median_audit.js
+++
@@ -1,9 +0,0 @@
-function median_webaudit() {
-  var d=document,s=screen?screen.width+'x'+screen.height:"";
-  var u=d.URL?new String(d.URL):"";
-  var r=d.referrer?new String(d.referrer):"";
-  return "@s="+s+"@u="+escape(u.substring(0,183))+"@r="+escape(r.substring(0,127));
-}
-var same=Math.floor(Math.random()*100000000)+median_webaudit();
-
-
