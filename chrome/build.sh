e1a18fb2ccafb79d6139006892d3986fcffba1ce
Lajos Koszti <ajnasz@ajnasz.hu>
2015-02-23 17:56:32 +0100

    es6 to es5 in chrome

--- chrome/build.sh
+++ chrome/build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-cat data/core/rq.js data/core/dom.js data/core/func.js data/core/commenttree.js data/core/comments.js data/core/articles.js data/core/blocks.js data/core/hupper-block.js data/core/unlimitedlinks.js data/hupper.js > data/built.js
+cat data/core/rq.js data/core/dom.js data/core/func.js data/core/commenttree.js data/core/comments.js data/core/articles.js data/core/blocks.js data/core/hupper-block.js data/core/unlimitedlinks.js data/hupper.js > data/built-es6.js
+babel data/built-es6.js > data/built.js
