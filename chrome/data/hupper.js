9f05dc4c80f3e404db22c01d07b50fe1ec9c2ee9
Lajos Koszti <ajnasz@ajnasz.hu>
2015-08-16 16:52:31 +0200

    Introduce events api, similar to ff

--- chrome/data/hupper.js
+++ chrome/data/hupper.js
@@ -6,6 +6,52 @@
 	let modBlocks = req('blocks');
 	let modArticles = req('articles');
 	let modHupperBlock = req('hupper-block');
+	let modComment = req('comment');
+
+	let events = (function () {
+		let listeners = new Map();
+
+		function listen(request, sender) {
+			let event = request.event;
+			let data = request.data;
+
+			if (listeners.has(event)) {
+				listeners.get(event).forEach((cb) => {
+					cb(data);
+				});
+			}
+
+			console.log('message request', request, sender);
+		}
+
+		return {
+			on(name, cb) {
+				console.log('Add listener', name);
+
+				if (!listeners.has(name)) {
+					listeners.set(name, []);
+				}
+
+				listeners.get(name).push(cb);
+			},
+
+			emit(name, args) {
+				console.log('Emit Listener', name);
+				chrome.runtime.sendMessage({'event': name, data: args});
+			},
+
+			init() {
+				console.log('events init');
+				chrome.runtime.onMessage.addListener(listen);
+				window.addEventListener('unload', function () {
+					listeners.clear();
+					listeners = null;
+				});
+
+			}
+
+		};
+	}());
 
 	function addHupperBlock() {
 		return new Promise(function (resolve) {
@@ -14,7 +60,7 @@
 			document.getElementById('block-hupper').addEventListener('click', function (e) {
 				let event = modBlocks.onBlockControlClick(e);
 				if (event) {
-					chrome.runtime.sendMessage({'event': 'block.action', data: event});
+					events.emit('block.action', event);
 				}
 			}, false);
 			resolve();
@@ -25,7 +71,7 @@
 		let articles = modArticles.parseArticles();
 
 		if (articles.length > 0) {
-			chrome.runtime.sendMessage({'event': 'gotArticles', data: articles});
+			events.emit('gotArticles', articles);
 		}
 	}
 
@@ -44,40 +90,33 @@
 			return;
 		}
 
-		let modComment = req('comment');
 		let modCommentTree = req('commenttree');
 
 		console.log('subscribe');
 		document.querySelector('body').addEventListener('click', modComment.onBodyClick, false);
 
 		commentsContainer.addEventListener('click', modComment.onCommentsContainerClick, false);
-		chrome.runtime.sendMessage({
-			event: 'gotComments',
-			data: modComment.convertComments(modCommentTree.getCommentTree(), options)
-		});
+		events.emit('gotComments', modComment.convertComments(modCommentTree.getCommentTree(), options));
 
 	}
 
 	function onGetBlocks() {
-		chrome.runtime.sendMessage({
-			event: 'gotBlocks',
-			data: modBlocks.getBlocks()
-		});
+		events.emit('gotBlocks', modBlocks.getBlocks());
 
 	}
 
 	function onCommentSetNew(newComments) {
-		req('comment').onCommentSetNew(newComments);
+		modComment.onCommentSetNew(newComments);
 	}
 
 	function onCommentAddNextPrev(item) {
-		req('comment').onCommentAddNextPrev(item);
+		modComment.onCommentAddNextPrev(item);
 	}
 
 	function onBlocakChangeOrderAll(data) {
 		modBlocks.reorderBlocks(data);
 		setTimeout(function () {
-			chrome.runtime.sendMessage({event: 'blocks.change-order-all-done'});
+			events.emit('blocks.change-order-all-done');
 		}, 5);
 	}
 
@@ -127,141 +166,43 @@
 
 	function onEnableBlockControls(blocks) {
 		modBlocks.onEnableBlockControls(blocks, function (event) {
-			chrome.runtime.sendMessage({event: 'block.action', data: event});
+			events.emit('block.action', event);
 		});
 	}
-
-	window.addEventListener('DOMContentLoaded', function () {
-		chrome.runtime.onMessage.addListener(function (request, sender) {
-			let event = request.event;
-			let data = request.data;
-			switch (event) {
-				case 'getArticles':
-					onGetArticles(data);
-				break;
-
-				case 'getComments':
-					onGetComments(data);
-				break;
-
-				case 'getBlocks':
-					onGetBlocks(data);
-				break;
-
-				case 'comments.update':
-					req('comment').onCommentUpdate(data);
-				break;
-
-				case 'comment.setNew':
-					onCommentSetNew(data);
-				break;
-
-				case 'comment.addNextPrev':
-					onCommentAddNextPrev(data);
-				break;
-
-				case 'comment.addParentLink':
-					req('comment').addParentLinkToComments(data);
-				break;
-
-				case 'comment.addExpandLink':
-					req('comment').addExpandLinkToComments(data);
-				break;
-
-				case 'articles.mark-new':
-					modArticles.onMarkNew(data);
-				break;
-
-				case 'articles.addNextPrev':
-					modArticles.onArticleAddNextPrev(data);
-				break;
-
-				case 'articles.add-category-hide-button':
-					onAddCategoryHideButton(data);
-				break;
-
-				case 'articles.hide':
-					onArticlesHide(data);
-				break;
-
-				case 'block.hide':
-					onBlockHide(data);
-				break;
-
-				case 'enableBlockControls':
-					onEnableBlockControls(data);
-				break;
-
-				case 'block.show':
-					onBlockShow(data);
-				break;
-
-				case 'block.hide-content':
-					onBlockHideContent(data);
-				break;
-
-				case 'block.show-content':
-					onBlockShowContent(data);
-				break;
-
-				case 'blocks.change-order-all':
-					onBlocakChangeOrderAll(data);
-				break;
-
-				case 'block.change-order':
-					onBlockChangeOrder(data);
-				break;
-
-				case 'block.change-column':
-					onBlockChangeColunn(data);
-				break;
-
-				case 'blocks.set-titles':
-					onBlockSetTitles(data);
-				break;
-
-				case 'trolluser':
-					onTrollUser(data);
-				break;
-
-				case 'untrolluser':
-					onUntrollUser(data);
-				break;
-
-				case 'highlightuser':
-					onHighlightUser(data);
-				break;
-
-				case 'unhighlightuser':
-					onUnhighlightUser(data);
-				break;
-
-				case 'hupper-block.add-menu':
-					modHupperBlock.addMenuItem(data);
-				break;
-
-				case 'hupper-block.hide-block':
-					modHupperBlock.addHiddenBlock(data);
-				break;
-
-				case 'hupper-block.show-block':
-					modHupperBlock.removeHiddenBlock(data);
-				break;
-
-				case 'setUnlimitedLinks':
-					req('unlimitedlinks').setUnlimitedLinks();
-				break;
-
-				default:
-					console.info('Missing event handler for %s', event);
-				break;
-			}
-			console.log('message request', request, sender);
-		});
+	function onDomContentLoaded() {
+		events.init();
+		events.on('getArticles', onGetArticles);
+		events.on('getComments', onGetComments);
+		events.on('getBlocks', onGetBlocks);
+		events.on('comments.update', modComment.onCommentUpdate);
+		events.on('comment.setNew', onCommentSetNew);
+		events.on('comment.addNextPrev', onCommentAddNextPrev);
+		events.on('comment.addParentLink', modComment.addParentLinkToComments);
+		events.on('comment.addExpandLink', modComment.addExpandLinkToComments);
+		events.on('articles.mark-new', modArticles.onMarkNew);
+		events.on('articles.addNextPrev', modArticles.onArticleAddNextPrev);
+		events.on('articles.add-category-hide-button', onAddCategoryHideButton);
+		events.on('articles.hide', onArticlesHide);
+		events.on('block.hide', onBlockHide);
+		events.on('enableBlockControls', onEnableBlockControls);
+		events.on('block.show', onBlockShow);
+		events.on('block.hide-content', onBlockHideContent);
+		events.on('block.show-content', onBlockShowContent);
+		events.on('blocks.change-order-all', onBlocakChangeOrderAll);
+		events.on('block.change-order', onBlockChangeOrder);
+		events.on('block.change-column', onBlockChangeColunn);
+		events.on('blocks.set-titles', onBlockSetTitles);
+		events.on('trolluser', onTrollUser);
+		events.on('untrolluser', onUntrollUser);
+		events.on('highlightuser', onHighlightUser);
+		events.on('unhighlightuser', onUnhighlightUser);
+		events.on('hupper-block.add-menu', modHupperBlock.addMenuItem);
+		events.on('hupper-block.hide-block', modHupperBlock.addHiddenBlock);
+		events.on('hupper-block.show-block', modHupperBlock.removeHiddenBlock);
+		events.on('setUnlimitedLinks', req('unlimitedlinks').setUnlimitedLinks);
 
 		modArticles.listenToTaxonomyButtonClick((articleStruct) => {
-
-			chrome.runtime.sendMessage({event: 'article.hide-taxonomy', data: articleStruct});
+			events.emit('article.hide-taxonomy', articleStruct);
 		});
 
 		addHupperBlock().then(function () {
@@ -269,10 +210,12 @@
 		});
 
 		console.log('dom content loaded');
-		chrome.runtime.sendMessage({'event': 'DOMContentLoaded'});
-	}, false);
+		events.emit('DOMContentLoaded');
+		// window.removeEventListener('DOMContentLoaded', onDomContentLoaded); // run once
+	}
+	window.addEventListener('DOMContentLoaded', onDomContentLoaded, false);
 
 	window.addEventListener('unload', function () {
-		chrome.runtime.sendMessage({'event': 'unload'});
+		events.emit('unload');
 	});
 }(window.req));
