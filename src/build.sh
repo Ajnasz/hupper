94f1ed54b284d0f62b1663d0f32e04f9c528199f
Lajos Koszti <ajnasz@ajnasz.hu>
2016-11-01 15:52:01 +0100

    Create zip for chrome

--- src/build.sh
+++ src/build.sh
@@ -2,13 +2,16 @@
 
 BROWSERIFY=$(npm bin)/browserify
 
-echo $BROWSERIFY
-
-echo start build
+echo -n " ."
 $BROWSERIFY options/main.js -o options/bundle.js -t [ babelify --presets [ es2015 ] ]
 echo -n " ."
 $BROWSERIFY data/main.js -o data/bundle.js -t [ babelify --presets [ es2015 ] ]
 echo -n " ."
 $BROWSERIFY lib/main.js -o lib/bundle.js -t [ babelify --presets [ es2015 ] ]
+echo -n " ."
+rm -f hupper.zip
+echo -n " ."
+zip --quiet -r hupper.zip data lib options options.html manifest.json package.json
 echo " . "
-echo "done `date`"
+
+echo "build on `date`"
