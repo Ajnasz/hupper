93dd87ee3fe5d7a56eb64299d44ea3faf30c0474
Lajos Koszti <ajnasz@ajnasz.hu>
2016-11-01 15:45:58 +0100

    manifest and package.json updates, local browserify

--- src/build.sh
+++ src/build.sh
@@ -1,4 +1,14 @@
-browserify options/main.js -o options/bundle.js -t [ babelify --presets [ es2015 ] ]
-browserify data/main.js -o data/bundle.js -t [ babelify --presets [ es2015 ] ]
-browserify lib/main.js -o lib/bundle.js -t [ babelify --presets [ es2015 ] ]
-echo "built" `date`
+#!/bin/sh
+
+BROWSERIFY=$(npm bin)/browserify
+
+echo $BROWSERIFY
+
+echo start build
+$BROWSERIFY options/main.js -o options/bundle.js -t [ babelify --presets [ es2015 ] ]
+echo -n " ."
+$BROWSERIFY data/main.js -o data/bundle.js -t [ babelify --presets [ es2015 ] ]
+echo -n " ."
+$BROWSERIFY lib/main.js -o lib/bundle.js -t [ babelify --presets [ es2015 ] ]
+echo " . "
+echo "done `date`"
