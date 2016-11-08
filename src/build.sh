0bd2d398b36b308f9d4cb63a7ce95098bb607a4a
Lajos Koszti <ajnasz@ajnasz.hu>
2016-11-08 08:35:18 +0100

    Better list of what needs to be in package

--- src/build.sh
+++ src/build.sh
@@ -9,9 +9,10 @@ $BROWSERIFY data/main.js -o data/bundle.js -t [ babelify --presets [ es2015 ] ]
 echo -n " ."
 $BROWSERIFY lib/main.js -o lib/bundle.js -t [ babelify --presets [ es2015 ] ]
 echo -n " ."
-rm -f hupper.zip
+rm -f hupper*.zip
 echo -n " ."
-zip --quiet -r hupper.zip icons/*.png data/bundle.js data/core/css/*.css lib/bundle.js options/bundle.js options/*.css options.html manifest.json package.json
+
+zip --quiet -r hupper.zip icons/{128,16,32,48}.png data/bundle.js data/core/css/*.css lib/bundle.js options/bundle.js options/*.css options.html manifest.json
 echo " . "
 
 echo "build on `date`"
