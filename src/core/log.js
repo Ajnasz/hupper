685223957dae1f2c0f4bbb84f977abab5b86ae10
Lajos Koszti <ajnasz@ajnasz.hu>
2016-11-12 13:09:51 +0100

    Fix indentation

--- src/core/log.js
+++ src/core/log.js
@@ -1,10 +1,12 @@
-let log = new Proxy({}, {get: function (target, name) {
-	return function () {};
-	if (console[name]) {
-		return console[name].bind(console);
-	}
+let log = new Proxy({}, {
+	get: function (target, name) {
+		return function () {};
+		if (console[name]) {
+			return console[name].bind(console);
+		}
 
-	return;
-}});
+		return;
+	}
+});
 
 export {log};
