4c83d3d1f9994046daf96c7bb18b8fa001efd9c0
Lajos Koszti <ajnasz@ajnasz.hu>
2016-12-03 15:21:20 +0100

    Remove es2015 babelify preset
    Hopefully everyone using a new browser

--- src/build.sh
+++ src/build.sh
@@ -5,11 +5,11 @@ set -e
 BROWSERIFY=$(npm bin)/browserify
 
 echo -n " ."
-$BROWSERIFY options/main.js -o options/bundle.js -t [ babelify --presets [ es2015 ] ]
+$BROWSERIFY options/main.js -o options/bundle.js -t babelify
 echo -n " ."
-$BROWSERIFY data/main.js -o data/bundle.js -t [ babelify --presets [ es2015 ] ]
+$BROWSERIFY data/main.js -o data/bundle.js -t babelify
 echo -n " ."
-$BROWSERIFY lib/main.js -o lib/bundle.js -t [ babelify --presets [ es2015 ] ]
+$BROWSERIFY lib/main.js -o lib/bundle.js -t babelify
 echo -n " ."
 rm -f hupper*.zip
 echo -n " ."
