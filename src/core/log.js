f65ab2b5b119676092882a9d2be2a0ad929dc620
Lajos Koszti <ajnasz@ajnasz.hu>
2016-12-17 18:15:25 +0100

    Don't log by default, allow to set logger

--- src/core/log.js
+++ src/core/log.js
@@ -1,12 +1,17 @@
-let log = new Proxy({}, {
-	get: function (target, name) {
-		return function () {};
-		if (console[name]) {
-			return console[name].bind(console);
+function nullFunction () {}
+
+let log = new Proxy({enabled: false, logger: null}, {
+	get (target, name) {
+		if (target.logger && target.logger[name]) {
+			if (target.enabled) {
+				return target.logger && target.logger[name].bind(target);
+			}
+
+			return nullFunction;
 		}
 
 		return;
 	}
 });
 
-export {log};
+export { log };
