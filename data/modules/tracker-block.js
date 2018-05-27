28e7cedd55a8827457fac79dac4ffa8976d71eff
Lajos Koszti <ajnasz@ajnasz.hu>
2018-05-27 17:58:27 +0200

    feat(tracker-block) Render only unless empty

--- data/modules/tracker-block.js
+++ data/modules/tracker-block.js
@@ -3,15 +3,21 @@ import modHupBlock from './hup-block';
 import getPage from './get-page';
 import userTrakcer from './tracker';
 
+const BLOCK_ID = 'block-hupper-user-tracker';
+const BLOCK_TITLE = 'User tracker';
+
 function create (user) {
 	return getPage(`/user/${user.id}/track`)
 		.then(page => userTrakcer.getContents(page.querySelector('#tracker')))
 		.then((f) => {
-			const block = modHupBlock.create('block-hupper-user-tracker', 'User tracker');
-			f.filter(f => f.answers.new > 0).forEach(f => modHupBlock.addMenuItem({
-				href: `${f.href}#new`,
-				text: `${f.title} (${f.answers.new} új)`,
-			}, dom.selectOne('.menu', block)));
+			const newAnswers = f.filter(f => f.answers.new > 0);
+			if (newAnswers.length) {
+				const block = modHupBlock.create(BLOCK_ID, BLOCK_TITLE);
+				newAnswers.forEach(f => modHupBlock.addMenuItem({
+					href: `${f.href}#new`,
+					text: `${f.title} (${f.answers.new} új)`,
+				}, dom.selectOne('.menu', block)));
+			}
 		});
 }
 
